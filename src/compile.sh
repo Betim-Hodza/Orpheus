2d281d205eeaf6fc2cc036ac488c99c16c9fccae
Betim H <bhodza02@gmail.com>
2025-07-21 19:12:15 -0500

    update: fixed compilation (using gnu gold to test heimdall)

--- src/compile.sh
+++ src/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # quick testing script
-gcc src/main.c -o orpheus -I/usr/include -lmpdclient -lncurses -llua5.3 -g
+gcc -fuse-ld=gold main.c -o orpheus -I/usr/include -lmpdclient -lncurses -llua -g
 
 ./orpheus
+
